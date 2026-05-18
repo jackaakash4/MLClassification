5557f5d77e3845d55d9d04035bd89b3cc541c7d9
Jack Aakash <jackaakash4@gmail.com>
2026-05-18 16:00:27 +0545

    Visualized some digits using matplotlib.pyplot

--- classification.py
+++ classification.py
@@ -1,4 +1,8 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 from sklearn.datasets import fetch_openml
+
 
 #fetching the MNIST dataset from OpenLM.org
 
@@ -8,4 +12,17 @@ X, y = mnist.data, mnist.target
 
 print("Shape of the minst dataset: \n", X.shape)
 
-print("\nX: \n", X)
+#print("\nX: \n", X)
+
+#Visualizing using maplotlib's imshow() function and using cmap = "binary" to get a grayscale color map where 0 is white and 255 is black
+
+def plot_digit(image_data):
+    image = image_data.reshape(28, 28)  #shapping it in  28 * 28 array
+    plt.imshow(image, cmap = 'binary')
+    plt.axis('off')
+    
+some_digit = X[0]
+plot_digit(some_digit)
+plt.show()
+
+
