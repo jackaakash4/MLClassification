6d528a7f75bf9f88969552e2a4280528d2a9197c
Jack Aakash <jackaakash4@gmail.com>
2026-05-21 22:26:00 +0545

    Use Stochastic Gradient Descent to classify and predict the number

--- classification.py
+++ classification.py
@@ -31,6 +31,29 @@ print("Digit is: \t", y[0])
 X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 
 #Training a binary classifiers
+#now only trying to identify only one digit i.e 5. so it is capable of distinguishing between just two classes 5 or not 5
+
+y_train_5 = (y_train == '5')        #True for all 5s, and False for all other digits
+y_test_5 = (y_test == '5')
+
+#starting SGD i.e stochastic gradient descent
+#creating SGD
+
+from sklearn.linear_model import SGDClassifier
+
+sgd_clf = SGDClassifier(random_state = 42)
+sgd_clf.fit(X_train, y_train_5)
+
+#now it is used to detect images of the number 5
+
+is5 = sgd_clf.predict([some_digit])       #some digit is 5 here
+
+print("Is it 5 ?", is5)
+
+#since it predited right
+#now measure the performance measure
+
+
 
 
 
